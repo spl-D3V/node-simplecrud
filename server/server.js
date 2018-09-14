7581f0144cf5a65936b809bd751989ffc86d5261
spl.D3V <spedraza.desarrollo@gmail.com>
2018-09-14 21:40:47 +0200

    tests postman

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ const bodyParse = require('body-parser')
 let {Mongoose} = require('./db/mongoosedb');
 let {Todo} = require('./models/todos');
 let {User} = require('./models/users');
+let {ObjectID} = require('mongodb');
+
+//beforeEach((done) =>{
+//    Todo.remove({}).then(()=> done());
+//});
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParse.json());
 app.post('/todos', (req, res) => {
     let todo = new Todo({
@@ -14,10 +20,33 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc)=>{
         res.send(doc);
     }, (e) =>{
+        res.status(400).send(e);
+    });
+});
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) =>{
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
         res.status(400).send();
     });
 });
 
-app.listen(3000, () => {
-    console.log('running');
+app.listen(port, () => {
+    console.log(`Runing in port : ${port}`);
 });
+
+module.exports = {app};
