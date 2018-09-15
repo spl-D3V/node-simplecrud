91e22995b00892b03253c463ad06a50363502354
spl.D3V <spedraza.desarrollo@gmail.com>
2018-09-15 18:06:56 +0200

    setup heroku

--- server/db/mongoosedb.js
+++ server/db/mongoosedb.js
@@ -1,4 +1,4 @@
 const Mongoose = require('mongoose');
 Mongoose.Promise = global.Promise;
-Mongoose.connect('mongodb://localhost:27017/TodoAppNuevo');
+Mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoAppNuevo');
 module.exports = {Mongoose}
