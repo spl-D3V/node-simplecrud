74baf8b16174f64e67c4e9747880b4b999828ee1
spl.D3V <spedraza.desarrollo@gmail.com>
2018-09-19 21:20:27 +0200

    bcrypt passw

--- server/models/users.js
+++ server/models/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
     email: { 
@@ -59,6 +60,19 @@ UserSchema.methods.generateAuthToken = function(){
         return token;
     });
 };
+UserSchema.pre('save', function(next){
+    let user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err, salt) =>{
+            bcrypt.hash(user.password, salt, (err, hash) =>{
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
+        next();
+    }
+});
 
 let User = mongoose.model('Users', UserSchema);
 module.exports = {User};
